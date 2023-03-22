c47fdbe7df3e308f3d0818a8e57dd567b7cfc3d3
KJM <kjm99d@naver.com>
2023-03-22 17:22:40 +0900

    ✨ 파이프통신 - 서버 코드 추가

---
+++ PipeServer.cpp
@@ -0,0 +1,60 @@
+﻿#include "PipeServer.h"
+
+
+CPipeServer::CPipeServer(std::wstring& wstrPipeName)
+{
+    std::wstring wstrPipePath(L"\\\\.\\pipe\\");
+    wstrPipePath.append(wstrPipeName);
+
+    m_pipe = CreateNamedPipeW(wstrPipePath.c_str(),
+        PIPE_ACCESS_DUPLEX,
+        PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
+        PIPE_UNLIMITED_INSTANCES,
+        1024 * 16,
+        1024 * 16,
+        0,
+        NULL);
+}
+
+void CPipeServer::Start()
+{
+    char buffer[1024];
+    while (true) {
+        DWORD bytesRead;
+        if (ReadFile(m_pipe, buffer, sizeof(buffer), &bytesRead, NULL) && bytesRead)
+        {
+            std::string message(buffer, bytesRead);
+            MessageReceivedCallback(message);
+        }
+        else
+        {
+            ErrorHandler();
+        }
+    }
+}
+
+void CPipeServer::SetMessageReceivedCallback(const std::function<void(const std::string&)>& callback)
+{
+    m_messageReceivedCallback = callback;
+}
+
+void CPipeServer::ErrorHandler()
+{
+    const DWORD dwErr = ::GetLastError();
+    switch (dwErr)
+    {
+    case ERROR_PIPE_LISTENING:
+    case ERROR_BROKEN_PIPE:
+        DisconnectNamedPipe(m_pipe);
+        ConnectNamedPipe(m_pipe, NULL);
+    default:
+        break;
+    }
+}
+
+void CPipeServer::MessageReceivedCallback(const std::string& message)
+{
+    if (m_messageReceivedCallback) {
+        m_messageReceivedCallback(message);
+    }
+}
---
+++ PipeServer.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include <Windows.h>
+#include <string>
+#include <functional>
+#pragma hdrstop
+
+/** :: Example Code 
+int main() {
+    std::wstring wstrPipeName(L"myfifo");
+    Server server(wstrPipeName);
+    server.SetMessageReceivedCallback(std::bind(&OnServerMessageReceived, std::placeholders::_1));
+    server.Start();
+    return 0;
+}
+ */
+
+/**
+ * \brief Named Pipe Server 클래스.
+ */
+class CPipeServer {
+public:
+    /**
+     * \brief Named Pipe Server 생성자.
+     * \param wstrPipeName Named Pipe 이름.
+     */
+    CPipeServer(std::wstring& wstrPipeName);
+
+    /**
+     * \brief Server 시작.
+     */
+    void Start();
+
+    /**
+     * \brief Message Received Callback 등록.
+     * \param callback 등록할 콜백 함수.
+     */
+    void SetMessageReceivedCallback(const std::function<void(const std::string&)>& callback);
+
+    /**
+     * \brief 에러 핸들러.
+     */
+    void ErrorHandler();
+
+private:
+    HANDLE m_pipe; ///< Named Pipe 핸들.
+    std::function<void(const std::string&)> m_messageReceivedCallback; ///< Message Received Callback.
+
+    /**
+     * \brief Message Received Callback.
+     * \param message 받은 메시지.
+     */
+    void MessageReceivedCallback(const std::string& message);
+};
